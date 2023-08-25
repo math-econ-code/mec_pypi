ec6b152a40d9a8ec166fbf7504213e2cbe03d603
Alfred Galichon <alfred.galichon@gmail.com>
2023-08-26 00:23:21 +0200

    v__0.0.2.3

--- mec/gt.py
+++ mec/gt.py
@@ -29,3 +29,27 @@ class Matrix_game:
 		p_i = S * xstar
 		q_j = S * ystar
 		return(p_i,q_j)
+
+class NashEquilibrium:
+	def __init__(self,A_i_j,B_i_j):
+		self.A_i_j = A_i_j
+		self.B_i_j = B_i_j
+		self.nbi,self.nbj = A_i_j.shape
+
+	def NashEquilibrium_zero_sum_solve(self):
+		model=grb.Model()
+		model.Params.OutputFlag = 0
+		model.params.NonConvex = 2
+		p_i = model.addMVar(shape=self.nbi)
+		q_j = model.addMVar(shape=self.nbj)
+		alpha = model.addMVar(shape = 1, lb = -grb.GRB.INFINITY)
+		beta = model.addMVar(shape = 1, lb = -grb.GRB.INFINITY)
+		model.setObjective(p_i@(self.A_i_j+ self.B_i_j)@q_j - alpha - beta,sense = grb.GRB.MAXIMIZE )
+		model.addConstr(self.A_i_j @ q_j - np.ones((self.nbi,1)) @  alpha <=  0 ) # 
+		model.addConstr(self.B_i_j.T @ p_i <= np.ones((self.nbj,1)) @  beta ) # @ 
+		model.addConstr(p_i.sum() == 1)
+		model.addConstr(q_j.sum() == 1)
+		model.optimize() 
+		thesol = np.array( model.getAttr('x'))
+		sol_dict = {'val1':thesol[-2], 'val2':thesol[-1], 'p_i':thesol[:self.nbi],'q_j':thesol[self.nbi:(self.nbi+self.nbj)]}    
+		return(sol_dict)
