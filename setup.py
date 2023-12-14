2178c54c194628e10a00b5df288f97625798491e
Alfred Galichon <alfred.galichon@gmail.com>
2023-12-14 17:39:48 +0100

    v__0.0.9.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name="mec",
-	version="0.0.9.2",
+	version="0.0.9.3",
 	authors=["Alfred Galichon"],
 	author_email="ag133@nyu.edu",
 	licence="",
