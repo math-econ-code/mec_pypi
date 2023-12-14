2c879f77f83285a1c713b8abe62b6e6e27f6b4e5
Antoine Jacquet <72971923+antoine-jacquet@users.noreply.github.com>
2023-12-14 13:32:55 +0100

    v__0.0.8.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name="mec",
-	version="0.0.8.8",
+	version="0.0.8.10",
 	authors=["Alfred Galichon"],
 	author_email="ag133@nyu.edu",
 	licence="",
